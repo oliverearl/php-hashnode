995b63f2cd84353e5ea612fcb3694b1a6fd82e70
Oliver Earl <oliver@oliverearl.co.uk>
2020-12-27 19:13:06 +0000

    Refactor urepo test, new tests

--- tests/UserRepositoryTest.php
+++ tests/UserRepositoryTest.php
@@ -2,6 +2,7 @@
 
 namespace Hashnode\Tests;
 
+use Hashnode\Api\SocialMedia;
 use PHPUnit\Framework\TestCase;
 
 use Hashnode\Api\User;
@@ -9,16 +10,47 @@ use Hashnode\Api\Repositories\UserRepository;
 
 class UserRepositoryTest extends TestCase
 {
+    private UserRepository $repository;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->repository = new UserRepository();
+    }
+
     /**
      * @covers ::User, ::UserRepository
      */
-    public function test_get_a_user_by_their_username(): void
+    public function test_a_user_can_be_retrieved_with_their_username(): void
     {
         $username = 'oliverearl';
-        $repository = new UserRepository();
-        $user = $repository->getUser($username);
+        $user = $this->repository->getUser($username);
 
         $this->assertInstanceOf(User::class, $user);
-        $this->assertEquals($username, $user->getUsername());
+        $this->assertEqualsIgnoringCase($username, $user->getUsername());
+    }
+
+    /**
+     * @covers ::User, ::UserRepository, ::SocialMedia
+     */
+    public function test_a_users_social_media_can_be_retrieved_with_their_username(): void
+    {
+        $username = 'eleftheriabatsou';
+        $user = $this->repository->getUser($username, true, false, false);
+
+        $this->assertEqualsIgnoringCase($username, $user->getUsername());
+        $this->assertInstanceOf(SocialMedia::class, $user->getSocialMedia());
+    }
+
+    /**
+     * @covers ::User, ::UserRepository
+     */
+    public function test_a_users_followers_can_be_retrieved_with_their_username(): void
+    {
+        $username = 'catalinpit';
+        $user = $this->repository->getUser($username, false, false, true);
+
+        $this->assertEqualsIgnoringCase($username, $user->getUsername());
+        $this->assertIsArray($user->getFollowers());
     }
 }
