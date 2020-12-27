4cf7c8d93d59d46226d94c8e16adc3b98fb4b1e6
Oliver Earl <oliver@oliverearl.co.uk>
2020-12-27 04:00:44 +0000

    Try/catch

--- lib/API/Repositories/UserRepository.php
+++ lib/API/Repositories/UserRepository.php
@@ -5,48 +5,55 @@ namespace Hashnode\Api\Repositories;
 use GraphQL\InlineFragment;
 use GraphQL\Query;
 use Hashnode\Api\User;
+use Hashnode\Exceptions\QueryException;
 
 class UserRepository extends Repository
 {
-    public function getUser(string $username): User
+    public function getUser(string $username): ?User
     {
-        $graphql = (new Query('user'))
-            ->setArguments(['username' => $username])
-            ->setSelectionSet(
-                [
-                    '_id',
-                    'name',
-                    'username',
-                    'tagline',
-                    'isEvangelist',
-                    'dateJoined',
-                    (new Query('socialMedia'))->setSelectionSet(
-                        [
-                            'twitter',
-                            'github',
-                            'stackoverflow',
-                            'linkedin',
-                            'google',
-                            'website',
-                            'facebook'
-                        ],
-                    ),
-                    'numFollowing',
-                    'numFollowers',
-                    'isDeactivated',
-                    'location',
-                    'photo',
-                    'coverImage',
-                    'numPosts',
-                    'numReactions',
-                    //'publication',
-                    'blogHandle',
-                    'publicationDomain',
-                    //'followers',
-                ]
-            );
-        $results = $this->client->getRawAccess()->runQuery($graphql, true)->getData();
-        return new User($results);
+        try {
+            $graphql = (new Query('user'))
+                ->setArguments(['username' => $username])
+                ->setSelectionSet(
+                    [
+                        '_id',
+                        'name',
+                        'username',
+                        'tagline',
+                        'isEvangelist',
+                        'dateJoined',
+                        (new Query('socialMedia'))->setSelectionSet(
+                            [
+                                'twitter',
+                                'github',
+                                'stackoverflow',
+                                'linkedin',
+                                'google',
+                                'website',
+                                'facebook'
+                            ],
+                        ),
+                        'numFollowing',
+                        'numFollowers',
+                        'isDeactivated',
+                        'location',
+                        'photo',
+                        'coverImage',
+                        'numPosts',
+                        'numReactions',
+                        //'publication',
+                        'blogHandle',
+                        'publicationDomain',
+                        //'followers',
+                    ]
+                );
+            $results = $this->client->getRawAccess()->runQuery($graphql, true)->getData();
+            return new User($results);
+        } catch (QueryException $exception) {
+            echo "Exception: {$exception->getMessage()}";
+            $exception->dump();
+            exit();
+        }
     }
 
     public function getUsers(): array
