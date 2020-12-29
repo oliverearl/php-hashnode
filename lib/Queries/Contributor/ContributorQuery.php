d342b698ce5c526b8da2494112f407bec7ec306d
Oliver Earl <oliver@oliverearl.co.uk>
2020-12-29 16:12:02 +0000

    Create ContributorQuery.php

---
+++ lib/Queries/Contributor/ContributorQuery.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Hashnode\Queries\Contributor;
+
+use Hashnode\Arguments\Contributor\UserArgument;
+use Hashnode\Queries\Query;
+use Hashnode\Queries\UserQuery;
+
+class ContributorQuery extends Query
+{
+    const OBJECT_NAME = 'Contributor';
+
+    public function selectUser(UserArgument $argsObject = null): UserQuery
+    {
+        $object = new UserQuery('user');
+
+        if (!is_null($argsObject)) {
+            $object->appendArguments($argsObject->toArray());
+        }
+
+        $this->selectField($object);
+
+        return $object;
+    }
+
+    public function selectStamp(): ContributorQuery
+    {
+        $this->selectField('stamp');
+
+        return $this;
+    }
+
+    public function selectId(): ContributorQuery
+    {
+        $this->selectField('id');
+
+        return $this;
+    }
+}
